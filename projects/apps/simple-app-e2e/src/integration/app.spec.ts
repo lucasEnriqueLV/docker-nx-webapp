d56064c9de3b2ccacb8b60b1636a21e9e7d1dc84
lucasELV <lelv@icomp.ufam.edu.br>
2022-03-18 23:07:42 -0400

    feat: Add individual component unit tests

--- projects/apps/simple-app-e2e/src/integration/app.spec.ts
+++ projects/apps/simple-app-e2e/src/integration/app.spec.ts
@@ -1,13 +1,32 @@
-import { getButton, getHeader, getInput } from '../support/app.po';
+import { getButtonAdd, getBoxItem, getInput, getTextInput } from './../support/app.po';
+import { getButtonComponent, getHeaderComponent, getInputComponent, getTitle } from '../support/app.po';
 
 describe('simple-app', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display three components', () => {
 
-    // Function helper example, see `../support/app.po.ts` file
-    getHeader().should('exist');
-    getButton().should('exist');
-    getInput().should('exist');
+    getHeaderComponent().should('exist');
+    getButtonComponent().should('exist');
+    getInputComponent().should('exist');
+  
   });
+
+  it('header should display simple-app', () => {
+    getTitle().should('exist');
+    getTitle().contains('simple-app');
+  })
+
+  it('button should add one', () => {
+    getButtonAdd().should('exist');
+    getBoxItem().contains('1');
+    getButtonAdd().click();
+    getBoxItem().contains('2');
+  })
+
+  it('input should display text after typing', () => {
+    getInput().should('exist');
+    getInput().type('hey this is a test!');
+    getTextInput().contains('hey this is a test!')
+  })
 });
--- projects/apps/simple-app-e2e/src/support/app.po.ts
+++ projects/apps/simple-app-e2e/src/support/app.po.ts
@@ -1,3 +1,11 @@
-export const getHeader = () => cy.get('projects-header.title');
-export const getButton = () => cy.get('projects-button.button');
-export const getInput = () => cy.get('projects-input.input');
+// Component checkers
+export const getHeaderComponent = () => cy.get('projects-header.title');
+export const getButtonComponent = () => cy.get('projects-button.button');
+export const getInputComponent = () => cy.get('projects-input.input');
+
+// Individual html tags
+export const getTitle = () => cy.get('h1.title');
+export const getButtonAdd = () => cy.get('button#buttonClickMe');
+export const getBoxItem = () => cy.get('div.box');
+export const getInput = () => cy.get('input.input');
+export const getTextInput = () => cy.get('div.text-from-input');
