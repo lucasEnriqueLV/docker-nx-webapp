f26f8bc498af47b6e2d788c151f800582235a2e7
lucasELV <lelv@icomp.ufam.edu.br>
2022-03-18 20:50:36 -0400

    feat: add e2e test battery

--- projects/apps/simple-app-e2e/src/support/app.po.ts
+++ projects/apps/simple-app-e2e/src/support/app.po.ts
@@ -1,1 +1,3 @@
-export const getGreeting = () => cy.get('h1');
+export const getHeader = () => cy.get('projects-header.title');
+export const getButton = () => cy.get('projects-button.button');
+export const getInput = () => cy.get('projects-input.input');
---
+++ projects/libs/ui-commons/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'ui-commons',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/libs/ui-commons',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
